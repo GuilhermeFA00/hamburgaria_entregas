9045c51e77854860e546ca3ed57c03eda384c82c
GuilhermeFA00 <sharkdevv@gmail.com>
2021-09-05 19:57:43 -0300

    scrool imgs done

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,27 @@
 //Scroll features products
-var imgScroll = document.getElementById('scroll-img').src;
+let imgScrool = document.getElementById('scroll-img');
+let returnScrool = document.getElementById('return-img')
+let imgHeader = document.getElementById('image-header');
+var clickCounter = 0;
 
+imgScrool.addEventListener('click', function () {
+    if (clickCounter >= 2) {
+        clickCounter = -1;
+        changeImg_next();
+    } if (clickCounter <= 2) {
+        clickCounter++;
+        changeImg_next();
+    }
+});
+
+function changeImg_next() {
+    if (clickCounter == 0) {
+        imgHeader.src = './src/imgs/hamb1.jpg';
+    }
+    if (clickCounter == 1) {
+        imgHeader.src = './src/imgs/hamb2.jpg';
+    }
+    if (clickCounter == 2) {
+        imgHeader.src = './src/imgs/hamb3.jpg';
+    }
+}
