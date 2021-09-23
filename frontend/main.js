1a28ff3598e5963be993469eeb9a7b092da5c2e7
GuilhermeFA00 <sharkdevv@gmail.com>
2021-09-22 22:16:55 -0300

    ups

--- frontend/main.js
+++ frontend/main.js
@@ -36,41 +36,49 @@ user_login();
 function user_login() {
     registerBtn.addEventListener('click', () => {
         let newHtml = `
-        <div class="signup-container">
-        <div class="signup-header">
-            <h2>Cadastre-se</h2>
-        </div>
-        <form id="sp-form" class="sp-form">
-            <button id="signup-exit"><i class="fas fa-times"></i></button>
-            <div class="sp-form-control">
-                <label for="username">Username</label>
-                <input type="text" placeholder="usuário18" id="username" />
-                <i class="fas fa-check-circle"></i>
-                <i class="fas fa-exclamation-circle"></i>
-                <small>Error message</small>
+        <div class="form_container">
+        <form class="form" id="login">
+            <h1 class="form__title">Login</h1>
+            <div class="form__message form__message--error"></div>
+            <div class="form__input-group">
+                <input type="text" class="form__input" autofocus placeholder="Username or email">
+                <div class="form__input-error-message"></div>
             </div>
-            <div class="sp-form-control">
-                <label for="username">Email</label>
-                <input type="email" placeholder="hello@xxx.com" id="email" />
-                <i class="fas fa-check-circle"></i>
-                <i class="fas fa-exclamation-circle"></i>
-                <small>Error message</small>
+            <div class="form__input-group">
+                <input type="password" class="form__input" autofocus placeholder="Password">
+                <div class="form__input-error-message"></div>
             </div>
-            <div class="sp-form-control">
-                <label for="username">Senha</label>
-                <input type="password" placeholder="Password" id="password" />
-                <i class="fas fa-check-circle"></i>
-                <i class="fas fa-exclamation-circle"></i>
-                <small>Error message</small>
+            <button class="form__button" type="submit">Continue</button>
+            <p class="form__text">
+                <a href="#" class="form__link">Forgot your password?</a>
+            </p>
+            <p class="form__text">
+                <a class="form__link" href="./" id="linkCreateAccount">Don't have an account? Create account</a>
+            </p>
+        </form>
+        <form class="form form--hidden" id="createAccount">
+            <h1 class="form__title">Create Account</h1>
+            <div class="form__message form__message--error"></div>
+            <div class="form__input-group">
+                <input type="text" id="signupUsername" class="form__input" autofocus placeholder="Username">
+                <div class="form__input-error-message"></div>
             </div>
-            <div class="sp-form-control">
-                <label for="username">Confirme a senha</label>
-                <input type="password" placeholder="Password two" id="password2" />
-                <i class="fas fa-check-circle"></i>
-                <i class="fas fa-exclamation-circle"></i>
-                <small>Error message</small>
+            <div class="form__input-group">
+                <input type="text" class="form__input" autofocus placeholder="Email Address">
+                <div class="form__input-error-message"></div>
             </div>
-            <button>Enviar</button>
+            <div class="form__input-group">
+                <input type="password" class="form__input" autofocus placeholder="Password">
+                <div class="form__input-error-message"></div>
+            </div>
+            <div class="form__input-group">
+                <input type="password" class="form__input" autofocus placeholder="Confirm password">
+                <div class="form__input-error-message"></div>
+            </div>
+            <button class="form__button" type="submit">Continue</button>
+            <p class="form__text">
+                <a class="form__link" href="./" id="linkLogin">Already have an account? Sign in</a>
+            </p>
         </form>
     </div>
         `
